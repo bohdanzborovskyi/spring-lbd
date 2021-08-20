e7be925e9b6a670aca1c7c2af968169e505db965
Bohdan Zborovskyi <baga142536@gmail.com>
2021-08-20 10:14:57 +0200

    Adding new branch

---
+++ src/main/java/com/zbodya/Controllers/SprintController.java
@@ -0,0 +1,11 @@
+package com.zbodya.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/sprints")
+public class SprintController
+{
+
+}
